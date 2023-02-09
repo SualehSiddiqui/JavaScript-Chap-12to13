3957918313f0cacc2a82f20196532ad3659a1839
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-02-09 16:32:54 +0500

    JavaScript Assignment 4
    JavaScript Assignment 4 Chap # 12-13

---
+++ app.js
@@ -0,0 +1,90 @@
+// Chap # 12to13
+
+// Question #01
+// var char = prompt("Enter your charector");
+// var dub = char.charCodeAt();
+// if(dub >=65 && dub <= 90){
+//     document.write("Your charector is upper case");
+// }
+// else if(dub >= 97 && dub <= 122){
+//     document.write("Your charector is lower case");
+// }
+// else if(dub >= 0){
+//      document.write("Your charector is number");
+// }
+
+// Question #02
+// var num1 = +window.prompt("Enter first number");
+// var num2 = +window.prompt("Enter second number");
+
+// if(num1 > num2){
+//     document.write("The larger number is: " + num1);
+// }
+// else if(num1 < num2){
+//     document.write("The larger number is: " + num2);
+// }
+// else{
+//     document.write("Both numbers are same " + num1);
+// }
+
+// Question #03
+// var num1 = window.prompt("Enter number");
+
+// if(num1 > 0){
+//     document.write("Positive Integer");
+// }
+// else if(num1 < 0){
+//     document.write("Negative Integer");
+// }
+// else{
+//     document.write("Zero");
+// }
+
+// Question #04
+// var char = window.prompt("Enter a charector(i.e. string of length 1)");
+
+// if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//     document.write("Your charector is vowel");
+// }
+// else{
+//     document.write("Your charector is consonant");
+// }
+
+// Question #05
+// var realPass = "sualeh123"
+// var userPass = window.prompt("Enter your password");
+
+// if (realPass === userPass) {
+//     document.write("Correct! The password you entered matches the orignal password");
+// }
+// else {
+//     document.write("Incorrect password");
+// }
+
+// Question #06
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     document.write(greeting = "Good day");
+// }
+// else{
+//         document.write(greeting = "Good evening");
+// }
+
+// Question #07
+// var time = prompt("Enter time in 24 hour format");
+// if(time >= 0000 && time < 1200){
+//     document.write("Good morning");
+// }
+// else if(time >= 1200 && time < 1700){
+//     document.write("Good afternoon");
+// }
+// else if(time >= 1700 && time < 2100){
+//     document.write("Good evening");
+// }
+// else if(time >= 2100 && time < 2359){
+//     document.write("Good night");
+// }
+// else{
+//     document.write("Please enter valid time");
+// }
